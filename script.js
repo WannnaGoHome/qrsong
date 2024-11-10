4deba9abccfd1bf60434263707eb8d31c90ed88a
arynttai <akosya.akmaral@gmail.com>
2024-11-10 23:49:18 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper");
+const secondPage = document.querySelector(".second-page");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  wrapper.classList.add("hidden"); 
+  secondPage.classList.remove("hidden"); 
+  history.pushState({ page: 2 }, "Second Page", "#second"); 
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${randomX}px`;
+  noBtn.style.top = `${randomY}px`;
+});
+
+window.addEventListener("popstate", (event) => {
+  if (event.state && event.state.page === 2) {
+    wrapper.classList.add("hidden");
+    secondPage.classList.remove("hidden");
+  } else {
+    wrapper.classList.remove("hidden");
+    secondPage.classList.add("hidden");
+  }
+});
